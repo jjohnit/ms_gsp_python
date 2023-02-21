b10a384c738aabaa800a91a2e771e731707cca96
rkarim4 <117399933+rkarim4@users.noreply.github.com>
2023-02-21 13:46:27 -0600

    Merge with main
    Merge with main

--- ms_gsp.py
+++ ms_gsp.py
@@ -1,146 +1,128 @@
 # importing modules
 import re
-import copy
 
 
 # Initial pass to create initial candidate set
-# sorted_items = list of itemsets sorted by min support
-# sequences = list of all sequences
-def init_pass(sorted_items, seqs_list):
-    L=[] 
-    seqs1="".join(seqs)
+# sorted_items = list of items sorted by min support
+# seqs_list = list of all sequences
+# min_support = minimum support
+def init_pass(sorted_items, all_sequences, min_support):
+    init_candidate_set = []
+    print('Minimum support is ', min_support)
+    total_sequences = len(all_sequences)
+    # Find support count of each item by iterating through the sequences
+    support_counts = {}
     for item in sorted_items:
-        supcnt[item]=(seqs1.count(item)/total_cnt)
-    for seq in seqs_list:
-        L_grp=[]
-        for grp in seq:
-            L_grp_item=[]
-            min_sup=-1
-            for item in grp:
-                if min_sup==-1:
-                    if supcnt[item]>=mis[item]:
-                        L_grp_item.append(item)
-                        min_sup=mis[item]
-                    
-                else:
-                    if supcnt[item]>=min_sup:
-                        L_grp_item.append(item)
-            if L_grp_item!=[]:
-                L_grp.append(L_grp_item)
-        if L_grp!=[]:
-            L.append(L_grp)
-    print("L:")    
-    print(L)
-    return L
-        
-        
-# create a frequent item set
+        for seq in all_sequences:
+            for itemset in seq:
+                if(item in itemset):
+                    support_counts[item] = 1 if(support_counts.get(item) == None) else (support_counts[item] + 1)
+                    break                    
+    print('Support counts are ', support_counts)
+    # Convert the support count to percentage
+    for key in support_counts.keys():
+        support_counts[key] = support_counts[key] / total_sequences
+    # Add items with minimum support to the init_candidate_set
+    for item in sorted_items:
+        if support_counts[item] >= min_support:
+            init_candidate_set.append(item)
+
+    print("Initial candidate set is ", init_candidate_set)
+    return init_candidate_set, support_counts
+
+
+#region create initial frequent item set
 # items = list of itemset
 # min_supports = min support of items
-def initial_frequent_item_set(L, mis):
-    F1=[]
-    
-    for seq in L:
-        for grp in seq:
-            for item in grp:
-                if supcnt[item]>=mis[item]:
-                    F1.append(item)
-    F1=[*set(F1)]
-    print(F1)
-    return F1
+# def initial_frequent_item_set(candidate_set, min_supports, support_counts):
+#     freq_item_set = set()
+#     for seq in candidate_set:
+#         # Initial candidate set contains items instead of item sets
+#         if support_counts[item] >= min_supports[item]:
+#             freq_item_set.add(item)
+#     print("Frequent item set 1 is ", freq_item_set)
+#     return freq_item_set
+#endregion
 
 # Create the frequent item set using the candidate set and previous frequent set
 def frequent_item_set(candidate_set, previous_frequent_set):
     pass
 
-# Create lvl 2 candidate sequence 
-def lvl_2_candidate_gen(L):
-    C2=[]
-    for seq in L:
-        for grp in seq:
-            for item in grp:
-                if supcnt[item]>=mis[item]:
-                    for i in range(grp.index(item)+1,len(grp)):
-                        if ((supcnt[grp[i]]>=mis[item]) and (abs(supcnt[grp[i]]-supcnt[item])<=SDC)):
-                                      C2.append([[item,grp[i]]])
-                    for i in range(seq.index(grp)+1,len(seq)):
-                        for item2 in seq[i]:
-                            if ((supcnt[item2]>=mis[item]) and (abs(supcnt[item2]-supcnt[item])<=SDC)):
-                                      C2.append([[item],[item2]])
-                            
-    print('C2:')
-    C2.sort()
-    k=0
-    while k<len(C2) and k+1<len(C2):
-        if C2[k]==C2[k+1]:
-            del C2[k+1]
-        else:
-            k=k+1
-    print(C2)
-    return C2
+#region Create lvl 2 candidate sequence
+# def lvl_2_candidate_gen(L):
+#     C2 = []
+#     for seq in L:
+#         for grp in seq:
+#             for item in grp:
+#                 if supcnt[item] >= mis[item]:
+#                     for i in range(grp.index(item)+1, len(grp)):
+#                         if ((supcnt[grp[i]] >= mis[item]) and (abs(supcnt[grp[i]]-supcnt[item]) <= SDC)):
+#                             C2.append([[item, grp[i]]])
+#                     for i in range(seq.index(grp)+1, len(seq)):
+#                         for item2 in seq[i]:
+#                             if ((supcnt[item2] >= mis[item]) and (abs(supcnt[item2]-supcnt[item]) <= SDC)):
+#                                 C2.append([[item], [item2]])
+#     print('C2:')
+#     C2.sort()
+#     k = 0
+#     while k < len(C2) and k+1 < len(C2):
+#         if C2[k] == C2[k+1]:
+#             del C2[k+1]
+#         else:
+#             k = k+1
+#     print(C2)
+#     return C2
+#endregion
+
+# Create lvl 2 candidate sequence
+def lvl_2_candidate_gen(freq_item_set, sup_counts, sdc):
+    candidate_list = []
+    for i in range(len(freq_item_set) - 1):
+        for j in range(i + 1, len(freq_item_set)):
+            if(abs(sup_counts[freq_item_set[j]] - sup_counts[freq_item_set[i]]) <= sdc):
+                candidate_list += [[freq_item_set[i], freq_item_set[j]], [freq_item_set[i]], [freq_item_set[j]]]
+    print("Level 2 candidate list is ", candidate_list)
+    return candidate_list
 
 # for creating min support candidate sets
-def ms_candidate_gen(F_prev, mis):
-    C=[]
-    F_prev_copy = copy.deepcopy(F_prev)
-    print(F_prev_copy)
-    
-    for i in range(0,len(F_prev)):
-        for j in range(i,len(F_prev)):
-            print("\n\n")
-            print(F_prev[i])
-            print(F_prev[j])
-            '''
-            if F_prev[i][0][1:]==F_prev[j][len(F_prev[j])-1][0:len(F_prev[j])-1] 
-            and mis[F_prev[i][0][0]]<=mis[F_prev[j][len(F_prev[j])-1][len()]]:
-                pass
-            '''
-            F_prev_copy = copy.deepcopy(F_prev)
-            if F_prev_copy[i][0]!=[]:
-                temp1=F_prev_copy[i][0].pop(0)
-                if F_prev_copy[i][0]
-                print(temp1)
-            F_prev_copy = copy.deepcopy(F_prev)
-            if F_prev_copy[j][len(F_prev[j])-1]!=[]:
-                temp2=F_prev_copy[j][len(F_prev[j])-1].pop()
-                
-                print(temp2)
-                
-            if temp1==temp2:
-                print(F_prev[i],F_prev[j])
-            
-    
-        
-    
-            
-        
+def ms_candidate_gen(frequent_set, min_supports):
+    pass
 
+# Function for sorting the items based on the minimum support
+
+
+def sort_items(all_items, min_supports):
+    minsup_items = []
+    for item in all_items:
+        minsup_items.append(min_supports[item])
+    sorted_items = [i for _, i in sorted(zip(minsup_items, all_items))]
+    return sorted_items
 
 # MS GSP algorithm with params
 # sequences = list of all sequenses
 # min_supports = list of all minimum supports
-def ms_gsp(sequences, min_supports):
-    #Final result
-    F=[]
-    # sort the itemsets in the sequences based on the ms value to create 'sorted_itemsets'
-    #sorted_itemsets = []
-    sorted_itemsets=sort_itemsets(I)
-    # call init_pass(sorted_items, sequences) to generate initial candidate set
-    L = init_pass(sorted_itemsets, sequences)
+
+
+def ms_gsp(sequences, min_supports, all_items, sdc):
+    # sort the items set in the sequences based on the ms value to create 'sorted_itemsets'
+    sorted_items = sort_items(all_items, min_supports)
+    print('Items sorted based on minimum support: ', sorted_items)
+    # call init_pass(sorted_items, sequences, minumum support) to generate initial candidate set
+    init_candidate_list, support_counts = init_pass(sorted_items, sequences, min_supports[sorted_items[0]])
     # create frequent item set 1 with elements in candidate_sequence having min support
-    F1 = initial_frequent_item_set(L, mis)
-    F.append(F1)
-    k=1
-    while F[k-1]!=[]:
-        # create candidate for level 2
-        if k==1:
-            candidate = lvl_2_candidate_gen(L)
-        else:
-            candidate = ms_candidate_gen(F[k-1], mis)
-        
-            
-        F.append(candidate) #As of now, all candidates appended
-        k=k+1
+    # freq_item_set_1 = initial_frequent_item_set(init_candidate_list, min_supports, support_counts)
+    freq_item_set = []
+    for item in init_candidate_list:
+        # Initial candidate set contains items instead of item sets
+        if support_counts[item] >= min_supports[item]:
+            freq_item_set.append(item)
+    print("Frequent item set 1 is ", freq_item_set)
+    # Create level 2 candidate list
+    # Why can't we pass frequent item set 1 instead of initial candidate set,
+    # considering we are eliminating items based on support count in the function?
+    candidate_list = lvl_2_candidate_gen(freq_item_set, support_counts, sdc)
+
+
 '''
     frequent_items = frequent_item_set(candidate_sequence, frequent_items)
     while(len(frequent_items) > 0):
@@ -149,64 +131,78 @@ def ms_gsp(sequences, min_supports):
 '''
 
 
+# Pre-processing of data
+# File with sequences (eg: <{10, 40, 50}{40, 90}> <{20, 30}{70, 80}{20, 30, 70}>)
+# sequences_file=str(input('Enter sequences file name:'))
+sequences_file = 'data1.txt'
+# File minimum item supports (eg: MIS(10) = 0.45 MIS(20) = 0.30)
+# minsups_file=str(input('Enter minimum supports file name:'))
+minsups_file = 'para1.txt'
+# Open the file to read the lines
+f = open(minsups_file, "r")
+lines = f.readlines()
+# Dictionary for storing the minimum supports for each item
+min_supports = {}
+sdc = 0
 
-# Function for sorting the itemsets       
-def sort_itemsets(I):
-    minsup_items=[]
-    for item in I:
-        minsup_items.append(mis[item])
-    sorted_itemsets = [i for _,i in sorted(zip(minsup_items,I))]
-    return sorted_itemsets   
-    
-
-
-#Pre-processing of data
-para_file=str(input('Enter parameter file name:'))
-data_file=str(input('Enter data file name:'))
-f = open(para_file, "r")
-lines=f.readlines()
-mis={}
-SDC=0
-
+# Iterate through each line of the mminimum supports file to extract the minimum supports for every item
 for line in lines:
-    misRegex = re.compile(r'MIS\((\d+)\) = (\d+.\d+)')
-    mo=misRegex.search(line)
-    if mo!=None:
-        mis[mo.group(1)]=float(mo.group(2))
-    misRegex = re.compile(r'SDC = (\d+.\d+)')
-    mo=misRegex.search(line)
-    if mo!=None:
-        SDC=float(mo.group(1))
+    # Remove the spaces in each line to have a standard format
+    line = line.replace(' ', '')
+    # Regex to find the minimum supports. Group 1 is the item and group 2 is its minimum support
+    mis_regex = re.compile(r'MIS\((\d+)\)=(\d+.\d+)')
+    ms = mis_regex.search(line)
+    if ms != None:
+        # Group 1 is the item and group 2 is its minimum support
+        min_supports[ms.group(1)] = float(ms.group(2))
+        continue
+    # If the line donot contain MIS, then check for SDC
+    mis_regex = re.compile(r'SDC=(\d+.\d+)')
+    ms = mis_regex.search(line)
+    if ms != None:
+        sdc = float(ms.group(1))
 
 f.close()
+print('Minimum supports are ', min_supports)
+print('SDC is ', sdc)
 
-f=open(data_file,"r")
-seqs=f.readlines()
+f = open(sequences_file, "r")
+# Read each line containing seperate sequences
+lines = f.readlines()
+# Item set for storing all seperate item (eg: {10, 40, 50,...})
+all_items = set()
 
-I=[]
-seqs_list=[]
-mis_list=[]
-for seq in seqs:
-    i_list=[]
-    seqsRegex = re.compile(r'({.*})')
-    mo=seqsRegex.search(seq)
-    list1=mo.group(1).split('}');
-    list1.pop()
-    for i in list1:
-        item_list=[]
-        i=i[1:]
-        i1=i.split(',')
-        mis_list=[]
-        for i1_item in i1:
-            i1_item=i1_item.strip();
-            item_list.append(i1_item)
-            I.append(i1_item);
-            mis_list.append(mis[i1_item])
-        item_list = [i for _,i in sorted(zip(mis_list,item_list))]
-        i_list.append(item_list)
-    seqs_list.append(i_list)
-total_cnt=len(seqs)
-#print(seqs)
-I=[*set(I)]
-supcnt={}
-ms_gsp(seqs_list,mis)
+# List to store all sequences extracted from the file
+all_sequences = []
+for line in lines:
+    # sequence_of_sorted_items = []
+    seqs_regex = re.compile(r'({.*})')
+    # Extract the item sets in each sequence
+    item_sets = seqs_regex.search(line)
+    # Create the sequence as a list of item sets
+    sequence_txt = item_sets.group(1).split('}')
+    sequence = []
+    # Removing the empty string at the end
+    sequence_txt.pop()
+    for i in sequence_txt:
+        item_list = []
+        # Ignore the 1st char '{'
+        i = i[1:]
+        # For getting each item in the sequence
+        items = i.split(',')
+        # Create the item list as a list to be added to the sequence list
+        for item in items:
+            item = item.strip()
+            item_list.append(item)
+            all_items.add(item)
+        # Add the item list to the sequence
+        sequence.append(item_list)
+    # Add the sequence to all sequences
+    all_sequences.append(sequence)
+f.close()
+print('Final sequences are ', all_sequences)
+print('Final items are ', all_items)
+# total_cnt=len(lines)
+sequences_count = len(all_sequences)
+print('Count of sequences is ', sequences_count)
+ms_gsp(all_sequences, min_supports, all_items, sdc)
